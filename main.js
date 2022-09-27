208eb04978e7757624a31bdeb628bcab934e6b4c
SakibBinYeasin17@gmail.com <SakibBinYeasin17@gmail.com>
2022-09-27 19:31:43 +0600

    first-commit

--- main.js
+++ main.js
@@ -1,52 +1,46 @@
-console.log('Hello World!');
-
-setInterval(() =>{
+  setInterval(() => {
+  let days = document.getElementById('days');
   let hours = document.getElementById('hours');
   let minutes = document.getElementById('minutes');
   let seconds = document.getElementById('seconds');
-  let ampm = document.getElementById('ampm');
-  let days = document.getElementById('days');
-  let months = document.getElementById('months');
-  let years = document.getElementById('years');
+
+
+  let dd = document.getElementById('dd');
   let hh = document.getElementById('hh');
   let mm = document.getElementById('mm');
   let ss = document.getElementById('ss');
 
-  
-  let d = new Date();
-  let h = d.getHours();
-  let m = d.getMinutes();
-  let s = d.getSeconds();
-  let ap = h >= 12 ? "pm" : "am";
-  
-  let day = d.getDate();
-  let month = d.getMonth()+1;
-  let year = d.getFullYear();
-  
-  h = h >12 ? h-12 : h;
-  h= h<10? "0"+h : h;
-  m= m<10? "0"+m : m;
-  s= s<10? "0"+s : s;
-  
-  day= day<10? "0"+day : day;
-  month= month<10? "0"+month : month;
-  year= year<10? "0"+year : year;
-  
-  
-  
-  hours.innerHTML = h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
-  ampm.innerHTML = ap;
- 
- 
-  days.innerHTML ="Date : "+" " + day;
-  months.innerHTML = month;
-  years.innerHTML = year;
-  
-  
-  hh.style.strokeDashoffset = 295 - (295.5*h)/12;
-  mm.style.strokeDashoffset = 295 - (295.5*m)/60;
-  ss.style.strokeDashoffset = 295 - (296.5*s)/60;
-});
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
 
+
+
+  let date = new Date("oct 5,2022 20:00:00").getTime();
+  let now = new Date().getTime();
+  let gap = date - now;
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  d = d < 10 ? "0" + d : d;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+
+
+  days.innerHTML = d +" D";
+  hours.innerHTML = h + " H";
+  minutes.innerHTML = m + " M";
+  seconds.innerHTML = s + " S";
+
+
+
+dd.style.strokeDashoffset = 295 - (295/13)*d;
+hh.style.strokeDashoffset = 295 - (295 /24)*h;
+  mm.style.strokeDashoffset = 295 - (295 /60)*m;
+  ss.style.strokeDashoffset =295- (295.5 / 60) * s;
+});
